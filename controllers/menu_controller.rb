e1a79fb81089eb6f844c89b14c085cb8dcbb815c
dsick <sick@davesick.net>
2016-07-26 09:17:41 -0700

    updated calculator to new model

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -35,72 +35,20 @@ class MenuController
 
     print "Enter Calculation String: "
     calc_string = gets.chomp
-    calculate_string(calc_string)
+    calc = CalculatorString.new(calc_string)
+    calc.validate_string
+    calc.calculate
+  rescue ArgumentError => e
+    puts "There was an argument error: #{e}"
+  rescue Exception => e
+    puts "There was a general error: #{e}"
+  ensure
     main_menu
-
   end
 
-  def calculate_string(calc_array)
-    working_calc_array = validate_string(calc_array)
-    remaining_array = working_calc_array
-    while remaining_array.length != 1
-      expr = remaining_array[0..2].join 
-      cumulative_result = two_item_calc(expr)
-      prepare_next_equation(remaining_array)
-      remaining_array.unshift(cumulative_result)
-    end
-    puts "String calculated to: #{cumulative_result}" 
-  end
 
-  def validate_string(calc_string)
-    calc_array = []
-    calc_array = calc_string.split /([\W])/
-
-    case calc_array
-      when calc_array.length == 0
-        raise ArgumentError, "can't calculate on array of zero length"
-      when calc_array.length == 0
-        calc_array[0]
-      when calc_array.length == 2
-        raise ArgumentError, "can't calculate on array of zero length of 2"
-      when calc_array.length > 2
-        return calc_array
-    end
-    calc_array
-  end
-
-  def prepare_next_equation (remaining_array)
-    for i in 1..3 do
-      remaining_array.delete_at(0)
-    end
-  end
-
-  def two_item_calc(expr)
-
-    result = 0
-   
-    l_operand, op, r_operand = expr.partition(%r{[/*+-]})
-
-    if l_operand.to_f.is_a? ! Numeric 
-      puts "Invalid left operand in expression: #{l_operand}"
-    end
-
-    if r_operand.to_f.is_a? ! Numeric
-      puts "Invalid right operand in expression: #{r_operand}"
-    end
-
-    if op.empty?
-      raise ArgumentError, "Invalid operation or no operation in expression: #{expr}"
-    end
-    l_operand, r_operand = l_operand.to_f, r_operand.to_f
-
-    case op
-      when '/' then result = l_operand / r_operand
-      when '*' then result = l_operand * r_operand
-      when '+' then result = l_operand + r_operand
-      when '-' then result = l_operand - r_operand
-    end
-  end
+  
+  
 
 end
 
--- models/calculator_string.rb
+++ models/calculator_string.rb
@@ -1,25 +1,64 @@
 class CalculatorString
-  attr_accessor :calc_string
+  attr_accessor :calc_string, :calc_array
 
   def initialize(calc_string)
-    calc_array = []
-    calc_array = calc_string.split /([\W])/
-    validate_string(@calc_array)
+    @calc_string = calc_string
+    @calc_array = []
   end
 
-  def validate_string(calc_array)
-
-    case calc_array
-      when calc_array.length == 0 then raise ArgumentError, "can't calculate on array of zero length"
-      when calc_array.length == 0 then return calc_array[0]
-      when calc_array.length == 2 then raise ArgumentError, "can't calculate on array of zero length of 2"
-      when calc_array.length >= 3 then return calc_array
+  def validate_string
+    calc_array = calc_string.split /([\W])/
+    case calc_array.length
+      when 0 then raise ArgumentError, "can't calculate on array of zero length"
+      when 1 then return calc_array[0]
+      when 2 then raise ArgumentError, "can't calculate on array of zero length of 2"
+      else 
+        @calc_array = calc_array
+        calc_array
     end
   end
 
-  # begin
+  def two_item_calc(expr)
+
+    result = 0
+    #combiner expression and run below
+    l_operand, op, r_operand = expr.partition(%r{[/*+-]})
+
+    if op.empty?
+      raise ArgumentError, "Invalid operation or no operation in expression: #{expr}"
+    end
+    l_operand, r_operand = Float(l_operand), Float(r_operand)
+
+    case op
+      when '/' then result = l_operand / r_operand
+      when '*' then result = l_operand * r_operand
+      when '+' then result = l_operand + r_operand
+      when '-' then result = l_operand - r_operand
+    end
+  end
+
+  def calculate
+    remaining_array = self.calc_array
+    puts "calculate - remaining array: #{remaining_array}"
+      while remaining_array.length != 1
+        expr = remaining_array[0..2].join 
+      cumulative_result = two_item_calc(expr)
+      prepare_next_equation(remaining_array)
+      remaining_array.unshift(cumulative_result)
+      # puts "#{remaining_array}"
+    end
+    puts "The result is: #{cumulative_result}"
     
-  # rescue Exception => e
-  #   myresult2 = e
-  # end
+  
+  end
+
+
+
+  def prepare_next_equation(remaining_array)
+    for i in 1..3 do
+      remaining_array.delete_at(0)
+    end
+  end
+
+  
 end
